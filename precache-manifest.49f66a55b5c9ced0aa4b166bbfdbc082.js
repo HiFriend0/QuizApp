aa6dd59018f3899df435133fdcef154b121e89ee
Abdallah Bentaleb <bentalebabdallah@gmail.com>
2020-06-05 15:24:59 +0200

    Updates

---
+++ precache-manifest.49f66a55b5c9ced0aa4b166bbfdbc082.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c0430797ab804a78210d9ad5778b845e",
+    "url": "/QuizApp/index.html"
+  },
+  {
+    "revision": "c35a2bee76090d4efc65",
+    "url": "/QuizApp/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "01c5d5510b30297d3cf2",
+    "url": "/QuizApp/static/js/2.91e76022.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/QuizApp/static/js/2.91e76022.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c35a2bee76090d4efc65",
+    "url": "/QuizApp/static/js/main.9a98b9ab.chunk.js"
+  },
+  {
+    "revision": "e207c65058a440f1fa45",
+    "url": "/QuizApp/static/js/runtime-main.a516a34e.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/QuizApp/static/media/logo.5d5d9eef.svg"
+  }
+]);
